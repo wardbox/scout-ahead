aee067398fb393fd81ddea0ba46f4649a13ea13d
wardbox <empirebillboard@gmail.com>
2020-09-01 14:32:38 -0700

    start from scratch

--- scoutahead/team/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("<int:pk>/", views.team_profile, name='team_profile'),
-    path("<str:handle>/", views.player_detail, name='player_detail'),
-]
