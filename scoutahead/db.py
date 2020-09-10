705339e51f9fd4bde6b7a2dd6d53e53509a07417
wardbox <empirebillboard@gmail.com>
2020-09-10 00:36:38 -0700

    big champ find update
    returns champions in your main role along with alternatives

--- scoutahead/db.py
+++ scoutahead/db.py
@@ -7,69 +7,26 @@ from sqlalchemy.ext import mutable
 # Initialize the database
 db = SQLAlchemy()
 
-class JsonEncodedDict(db.TypeDecorator):
-    """Enables JSON storage by encoding and decoding on the fly."""
-    impl = db.Text
-
-    def process_bind_param(self, value, dialect):
-        if value is None:
-            return '{}'
-        else:
-            return json.dumps(value)
-
-    def process_result_value(self, value, dialect):
-        if value is None:
-            return {}
-        else:
-            return json.loads(value)
-
-
-mutable.MutableDict.associate_with(JsonEncodedDict)
-
-class Champion(db.Model):
+class ChampionRole(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=False)
-    hard_cc = db.Column(db.Boolean, default=False)
-    hard_engage = db.Column(db.Boolean, default=False)
-    disengage = db.Column(db.Boolean, default=False)
-    poke = db.Column(db.Boolean, default=False)
-    waveclear = db.Column(db.Boolean, default=False)
-    tank = db.Column(db.Boolean, default=False)
+    hard_cc = db.Column(db.String, nullable=True)
+    hard_engage = db.Column(db.String, nullable=True)
+    disengage = db.Column(db.String, nullable=True)
+    poke = db.Column(db.String, nullable=True)
+    waveclear = db.Column(db.String, nullable=True)
+    tank = db.Column(db.String, nullable=True)
     damage = db.Column(db.String, nullable=False)
-    early_game = db.Column(db.Boolean, default=False)
-    mid_game = db.Column(db.Boolean, default=False)
-    late_game = db.Column(db.Boolean, default=False)
-    red = db.Column(db.String, default=False)
-    green = db.Column(db.String, default=False)
-    blue = db.Column(db.String, default=False)
-    white = db.Column(db.String, default=False)
-    black = db.Column(db.String, default=False)
-    colorless = db.Column(db.String, default=False)
+    early_game = db.Column(db.String, nullable=True)
+    mid_game = db.Column(db.String, nullable=True)
+    late_game = db.Column(db.String, nullable=True)
+    red = db.Column(db.String, nullable=True)
+    green = db.Column(db.String, nullable=True)
+    blue = db.Column(db.String, nullable=True)
+    white = db.Column(db.String, nullable=True)
+    black = db.Column(db.String, nullable=True)
+    colorless = db.Column(db.String, nullable=True)
 
     # Create a function to return a string when we add something
     def __repr__(self):
         return f'<Name {self.name}>'
-
-class Match(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    gameId = db.Column(db.Integer, nullable=False)
-    role = db.Column(db.String, nullable=False)
-    season = db.Column(db.Integer, nullable=False)
-    platformId = db.Column(db.String, nullable=False)
-    champion = db.Column(db.Integer, nullable=False)
-    queue = db.Column(db.Integer, nullable=False)
-    lane = db.Column(db.String, nullable=False)
-    timestamp = db.Column(db.Integer, nullable=False)
-    position = db.Column(db.String, nullable=False)
-    date_created = db.Column(db.DateTime, default=datetime.utcnow)
-
-    def __repr__(self):
-        return f'<Name {self.gameId}>'
-
-class MatchDetail(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    object_args = db.Column(JsonEncodedDict)
-    gameId = db.Column(db.Integer, nullable=False)
-
-    def __repr__(self):
-        return f'<Name {self.id}>'
--- scoutahead/scoutahead.py
+++ scoutahead/scoutahead.py
@@ -1,12 +1,22 @@
 import cassiopeia as cass
-from cassiopeia import Summoner, Patch, Queue
+from cassiopeia import Summoner, Season, Queue, Champion, Champions, Patch, Position
 from collections import OrderedDict
 from operator import getitem, itemgetter
-import operator
+import operator, csv, roleml
+from scoutahead.db import *
+import datetime
+import bs4
+
+region = "NA"
 
 # Cassiopeia settings
 cass.apply_settings('cass_settings.json')
-cass.set_default_region("NA")
+cass.set_default_region(region)
+
+def testing():
+    champs = Champions()
+    for champion in champs:
+        print(champion.tags)
 
 def get_comps(summoners):
 
@@ -15,31 +25,63 @@ def get_comps(summoners):
         #    return None
         pass
 
+    #save_champ_role()
+    
     #bring back compositions
-    summoners = []
+    team_comps = []
 
     for player in team:
-        summoner = Summoner(name=player)
+        # Summoner specific data
+        summoner = Summoner(name=player, region=region)
         name = summoner.name
         profile_icon = summoner.profile_icon.id
-        matches = summoner.match_history(begin_time=Patch.from_str("10.10", region="NA").start, queues={Queue.ranked_solo_fives})
+        matches = summoner.match_history(begin_time=Patch.from_str("10.1").start, end_time=Patch.from_str("10.18").end, queues={Queue.ranked_solo_fives})
+        cms = cass.get_champion_masteries(summoner=summoner, region=region)
 
+        champion_id_to_name_mapping = {champion.id: champion.name for champion in cass.get_champions(region=region)}
         champions_played = {}
 
+        roles = {
+            'top': 0,
+            'jungle': 0,
+            'mid': 0,
+            'bot': 0,
+            'supp': 0
+        }
+
         for match in matches:
-            champion = match.participants[summoner].champion.name
+            # Append predicted role from roleML
+            match.timeline.load()
+            match_loaded = match.load()
+            if match.duration > datetime.timedelta(minutes=15):
+                roleml.add_cass_predicted_roles(match_loaded)
+                role = match.participants[summoner].predicted_role
+
+
+            champion_id = match.participants[summoner].champion.id
+            champion = champion_id_to_name_mapping[champion_id]
+            participant_id = match.participants[summoner].id
             won = match.participants[summoner].team.win
+            champion_obj = Champion(name=champion, id=champion_id)
+            mastery_champ = cms.filter(lambda cm: cm.champion == champion_obj)
+            mastery_level = ([cm.level for cm in mastery_champ])[0]
+            mastery_points = ([cm.points for cm in mastery_champ])[0]
 
             if won == True:
                 won = 1
             else:
                 won = 0
 
+            if role:
+                roles[role] += 1
+
             if champion not in champions_played.keys():
                 champions_played[champion] = {
                     'played': 1,
                     'won': won,
-                    'win_rate': 0
+                    'win_rate': 0,
+                    'mastery_level': mastery_level,
+                    'mastery_points': mastery_points
                 }
             else:
                 played = champions_played[champion]['played']
@@ -52,12 +94,149 @@ def get_comps(summoners):
             champions_played[champion]['win_rate'] = int(win_rate)
 
         # Sort by played then win rate
-        sorted_champions = sorted(champions_played.items(), key=lambda x: (x[1]['played'], x[1]['win_rate']), reverse=True)
+        sorted_champions = sorted(champions_played.items(), key=lambda x: (x[1]['played'], x[1]['mastery_points'], x[1]['win_rate']), reverse=True)
+        pro_champions = [champ for champ in sorted_champions if champ[1]['mastery_level'] > 5]
 
-        summoners.append({
-            'name': name,
-            'profile_icon': profile_icon,
-            'champions': sorted_champions
-        })
+        main_role = (max(roles, key=roles.get))
 
-    return summoners
+        if main_role == 'top':
+            main_role = 'TOP'
+        elif main_role == 'jungle':
+            main_role = 'JUNGLE'
+        elif main_role == 'mid':
+            main_role = 'MIDDLE'
+        elif main_role == 'bot':
+            main_role = 'BOTTOM'
+        else:
+            main_role = 'UTILITY'
+        
+        pro_champ_with_roles = []
+
+        champions = Champions()
+
+        # Retrieve mtg and team comp roles
+        for champion in pro_champions:
+
+            try:
+                play_rate = str(list((Champion(name=champion[0],region=region).play_rates).keys())[0]).upper().split('.')
+                play_rate = play_rate[1]
+                if play_rate not in main_role:
+                    continue
+            except:
+                print("============ fucked up", champ.name)
+                pass
+
+            cr = ChampionRole.query.filter_by(name=champion[0]).first()
+
+            queries = {}
+
+            # find similar champions based on strict similarity
+            for column in cr.__table__.columns:
+                v = str(getattr(cr, column.name))
+                # Match roles, power spikes, damage type, mtg color
+                if v == "TRUE" or v == 'PHYSICAL' or v == 'MAGICAL' or v == 'UTILITY' or v == 'O':        
+                    queries[column.name] = str(getattr(cr, column.name))
+
+            sim_champions = ChampionRole.query.filter_by(**queries).all()
+
+            # widen our search for those who have < 2
+            if len(sim_champions) < 3:
+                queries.clear()
+
+                for column in cr.__table__.columns:
+                    v = str(getattr(cr, column.name))
+                    # Match roles, damage type
+                    if v == "TRUE" or v == 'PHYSICAL' or v == 'MAGICAL' or v == 'UTILITY':
+                        queries[column.name] = str(getattr(cr, column.name))
+                
+                sim_champions = ChampionRole.query.filter_by(**queries).all()
+
+            # Widen search even further
+            if len(sim_champions) < 3:
+                queries.clear()
+
+                for column in cr.__table__.columns:
+                    v = str(getattr(cr, column.name))
+                    if v == 'TRUE' or v == 'PHYSICAL' or v == 'MAGICAL' or v == 'UTILITY':
+                        # Ignore power spikes
+                        if column.name != 'early_game' and column.name != 'mid_game' and column.name != 'late_game':
+                            queries[column.name] = str(getattr(cr, column.name))
+
+                sim_champions = ChampionRole.query.filter_by(**queries).all()
+
+            true_sim_champions = []
+
+            for champ in sim_champions:
+
+                kill_me = False
+                if champ.name == champion[0]:
+                    kill_me = True
+                elif champ.name not in cms:
+                    kill_me = True
+                    break
+
+                champ_ob = Champion(name=champ.name,region=region)
+                try:
+                    play_rate = str(list((champ_ob.play_rates).keys())[0]).upper().split('.')
+                    play_rate = play_rate[1]
+                    same_role = False
+
+                    if play_rate not in main_role:
+                        kill_me = True
+                except:
+                    print("============ fucked up", champ.name)
+                    pass
+                
+                if kill_me == False:
+                    true_sim_champions.append(champ)
+            
+            pro_champ_with_roles.append({
+                'name': champion[0],
+                'similar': true_sim_champions,
+                'roles': queries,
+                'win_rate': champion[1]['win_rate'],
+                'mastery_level': champion[1]['mastery_level'],
+                'mastery_points': champion[1]['mastery_points']
+            })
+
+        team_comps.append(
+            {
+                'name': summoner.name,
+                'profile_icon': summoner.profile_icon.id,
+                'role': main_role,
+                'champions': pro_champ_with_roles
+            }
+        )
+    return team_comps
+
+def save_champ_role():
+    with open('docs/MTG_COLORS.csv', newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+
+        for champion in reader:
+            
+            new_champion = ChampionRole(
+                name = champion['name'],
+                hard_cc = champion['hard_cc'],
+                hard_engage = champion['hard_engage'],
+                disengage = champion['disengage'],
+                poke = champion['poke'],
+                waveclear = champion['waveclear'],
+                tank = champion['tank'],
+                damage = champion['damage'],
+                early_game = champion['early_game'],
+                mid_game = champion['mid_game'],
+                late_game = champion['late_game'],
+                red = champion['red'],
+                green = champion['green'],
+                blue = champion['blue'],
+                white = champion['white'],
+                black = champion['black'],
+                colorless = champion['colorless']
+            )
+
+            try:
+                db.session.add(new_champion)
+                db.session.commit()
+            except:
+                print("Error saving to DB.")
